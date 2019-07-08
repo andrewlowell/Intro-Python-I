6d5a6d36821c672e226bc54327ec58698c457d75
Andrew Richardson <andrewlowellr@gmail.com>
2019-07-08 14:32:14 -0600

    Fix assignment 3.

--- src/03_modules.py
+++ src/03_modules.py
@@ -11,7 +11,9 @@ import sys
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
 
-print(sys.argv)
+args = sys.argv
+for tuppence in args:
+  print(tuppence)
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
